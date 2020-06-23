81bd320b31dc3bd348b4535ad74d048541ab87d1
stark <stark@aleph.engineering>
2020-06-23 12:57:01 -0400

    Refactor model constructor

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 
 class Todo extends ChangeNotifier {
-  String _id = '0';
-  String _title = 'titulo';
-  bool _done = false;
+  final String _id;
+  String _title;
+  bool _done;
 
-  Todo(this._id, this._title, this._done);
+  Todo({id, title,}): _id = id, _title = title, _done = false;
 
 
   String get id => _id;
